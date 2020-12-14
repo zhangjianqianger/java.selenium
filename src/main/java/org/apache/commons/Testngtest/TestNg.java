52bae8225fcff37b2d32396577b8e75e8c4ed55f
Administrator <Administrator@User-2018FETWAO>
2020-12-14 14:52:18 +0800

    chuangjian

---
+++ src/main/java/org/apache/commons/Testngtest/TestNg.java
@@ -0,0 +1,12 @@
+package org.apache.commons.Testngtest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+@Test
+public class TestNg {
+	public static void main(String[] args) {
+		System.out.println("这是一个简单验证");
+		  Assert.assertTrue(true);
+	}
+
+}
